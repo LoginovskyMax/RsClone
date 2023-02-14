e307a31748814d623cc6a7caf0a2b962e75e6e81
Alexey Kuptsov <jerubbaal@mail.ru>
2023-02-14 12:35:08 +0600

    feat: add Not Found Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 
 import "./App.scss";
-import LoginPage from "./Pages/LoginPage";
 import MainPage from "./Pages/MainPage";
 import MemoryGame from "./Pages/MemoryGame/MemoryGame";
+import NotFound from "./Pages/NotFound/NotFound";
 import PreviewPage from "./Pages/PreviewPage/PreviewPage";
 import { SeaBattle } from "./Pages/SeaBattle/SeaBattle";
 import StartPage from "./Pages/StartPage";
@@ -14,9 +14,10 @@ const App = () => (
       <Route path="/" element={<StartPage />} />
       <Route path="/main" element={<MainPage />} />
       <Route path="/Memorygame" element={<MemoryGame />} />
-      <Route path="/login" element={<LoginPage />} />
+      {/* <Route path="/login" element={<LoginPage />} /> */}
       <Route path="/preview/:game" element={<PreviewPage />} />
       <Route path="/SeaBattle" element={<SeaBattle />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </div>
 );
---
+++ src/Pages/NotFound/NotFound.tsx
@@ -0,0 +1,18 @@
+import "./style.scss";
+
+const NotFound = () => (
+  <div id="notfound">
+    <div className="notfound">
+      <div className="notfound-bg">
+        <div />
+        <div />
+        <div />
+      </div>
+      <h1>oops!</h1>
+      <h2>Error 404 : Page Not Found</h2>
+      <button className="button">Go Back</button>
+    </div>
+  </div>
+);
+
+export default NotFound;
