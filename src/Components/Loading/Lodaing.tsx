1fd027847319ee8ba14fb4e26bca01f827477906
Alexey Kuptsov <jerubbaal@mail.ru>
2023-02-27 11:10:38 +0600

    fix: loading styles

--- src/Components/Loading/Lodaing.tsx
+++ src/Components/Loading/Lodaing.tsx
@@ -1,13 +1,9 @@
-import { useEffect } from "react";
-
 import useStatusStore from "../../store/load-status";
 
 import "./style.scss";
 
 const Loading = () => {
   const status = useStatusStore();
-
-  useEffect(() => console.log("Loading..."), [status.isLoading]);
 
   return (
     <div className={`loading${status.isLoading ? "" : " loading_hide"}`}>
